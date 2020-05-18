7d5095e4316755ed1fab4250c5952d751da6a663
daodao1996 <yunpeng.ding@thoughtworks.com>
2020-05-18 17:46:04 +0800

    change timeout

--- src/main/scala/com/training/CopyFile.scala
+++ src/main/scala/com/training/CopyFile.scala
@@ -1,8 +1,10 @@
 package com.training
 
-import com.amazonaws.services.s3.model.CopyObjectResult
-
 class CopyFile extends S3{
-    def copy: CopyObjectResult =
+    def copy: String = {
+      println("copy start")
       copyFile("aws-training-dingyunpeng", "readme.txt", "copy-bucket-dyp", "copyReadme.txt")
+      println("copy finished")
+      "success"
+  }
 }
